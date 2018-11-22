be559f703e05ac53d17d3114e58142dd1ed41fc8
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-22 02:10:27 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,6 +1,6 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
-def 
+
 def times_iterator(number_of_times)
    number_of_times.times do 
     puts "Welcome to Flatiron School's Web Development Course!"

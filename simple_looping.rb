4ff63c15aba7a3eca145b3c8b372e4114a4d8cf4
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-22 02:23:04 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -34,7 +34,7 @@ end
 
 def for_iterator(number_of_times)
     
-     for number in 0..number_of_times
+     for number in 0..number_of_times do
          puts "Welcome to Flatiron School's Web Development Course!"
      end
 end
